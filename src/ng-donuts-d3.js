34ccb072ed3e117b18db60c0e284fa09e6763992
hugomosh <hugomosh@gmail.com>
2015-06-23 22:59:12 -0500

    d3

--- src/ng-donuts-d3.js
+++ src/ng-donuts-d3.js
@@ -1,18 +1,55 @@
 'use strict';
 
 angular.module('hugomosh.ngDonutsD3', [])
+    .directive('myDirective', function() {
 
-  .directive('myDirective', function() {
-
-    return {
-      restrict: 'EAC',
-      scope: true,
-      compile: function compile(tElement, tAttrs, transclude) {
-        tElement.html('<span>hello {{name}}</span>');
-        return function postLink(scope, iElement, iAttrs, controller) {
-          scope.name = 'world';
+        return {
+            restrict: 'EAC',
+            scope: true,
+            compile: function compile(tElement, tAttrs, transclude) {
+                tElement.html('<span>hello {{name}}</span>');
+                return function postLink(scope, iElement, iAttrs, controller) {
+                    scope.name = 'world';
+                };
+            }
         };
-      }
-    };
+    })
+    .directive('donoutChart', function() {
+        function link(scope, element, attrs) {
+            var format,
+                timeoutId;
 
-  });
+            function updateTime() {
+                element.text(dateFilter(new Date(), format));
+            }
+
+            scope.$watch(attrs.myCurrentTime, function(value) {
+                format = value;
+                updateTime();
+            });
+
+            element.on('$destroy', function() {
+                $interval.cancel(timeoutId);
+            });
+
+            // start the UI update process; save the timeoutId for canceling
+            timeoutId = $interval(function() {
+                updateTime(); // update DOM
+            }, 1000);
+        }
+
+        return {
+            restrict: 'EAC',
+            scope: {
+                value: '=',
+                total: '=',
+            },
+            template: '<span>{{value||50}} - {{total||100}}</span>'
+                /*compile: function compile(tElement, tAttrs, transclude) {
+                    tElement.html('<span>{{value||50}} {{total||100}}</span>');
+                    return function postLink(scope, iElement, iAttrs, controller) {
+                        scope.name = 'world';
+                    };
+                }*/
+        };
+    });
--- test/spec/ng-donuts-d3.js
+++ test/spec/ng-donuts-d3.js
@@ -1,43 +1,64 @@
 'use strict';
 
-describe('Module: ngDonutsD3', function () {
-  var scope, $sandbox, $compile, $timeout;
+describe('Module: ngDonutsD3', function() {
+    var scope, $sandbox, $compile, $timeout;
 
-  // load the controller's module
-  beforeEach(module('hugomosh.ngDonutsD3'));
+    // load the controller's module
+    beforeEach(module('hugomosh.ngDonutsD3'));
 
-  beforeEach(inject(function ($injector, $rootScope, _$compile_, _$timeout_) {
-    scope = $rootScope;
-    $compile = _$compile_;
-    $timeout = _$timeout_;
+    beforeEach(inject(function($injector, $rootScope, _$compile_, _$timeout_) {
+        scope = $rootScope;
+        $compile = _$compile_;
+        $timeout = _$timeout_;
 
-    $sandbox = $('<div id="sandbox"></div>').appendTo($('body'));
-  }));
+        $sandbox = $('<div id="sandbox"></div>').appendTo($('body'));
+    }));
 
-  afterEach(function() {
-    $sandbox.remove();
-    scope.$destroy();
-  });
+    afterEach(function() {
+        $sandbox.remove();
+        scope.$destroy();
+    });
 
-  var templates = {
-    'default': {
-      scope: {},
-      element: '<div my-directive></div>'
+    var templates = {
+        'default': {
+            scope: {},
+            element: '<div my-directive></div>'
+        },
+        'donout1': {
+            scope: {},
+            element: '<div donout-chart></div>'
+        },
+        'donout2': {
+            scope: {},
+            element: '<div donout-chart value="20" total="80"></div>'
+        }
+    };
+
+    function compileDirective(template) {
+        template = template ? templates[template] : templates['default'];
+        angular.extend(scope, template.scope || templates['default'].scope);
+        var $element = $(template.element).appendTo($sandbox);
+        $element = $compile($element)(scope);
+        scope.$digest();
+        return $element;
     }
-  };
 
-  function compileDirective(template) {
-    template = template ? templates[template] : templates['default'];
-    angular.extend(scope, template.scope || templates['default'].scope);
-    var $element = $(template.element).appendTo($sandbox);
-    $element = $compile($element)(scope);
-    scope.$digest();
-    return $element;
-  }
+    it('should correctly display hello world', function() {
+        var elm = compileDirective();
+        expect(elm.text()).toBe('hello world');
+    });
 
-  it('should correctly display hello world', function () {
-    var elm = compileDirective();
-    expect(elm.text()).toBe('hello world');
-  });
+    it('should display values of donout directive', function() {
+        var elm = compileDirective('donout1');
+        console.log(elm.isolateScope().total);
+    });
+
+    it('should have 3d library', function() {
+        var elm = compileDirective('donout2');
+
+        expect(elm.isolateScope().total).toBe(80);
+        expect(elm.isolateScope().value).toBe(20);
+        expect(elm.text()).toBe('20 - 80');
+    });
 
 });
